8f38eb01c3a3c393b128e05d954c4c15b40f7191
leandro <leandro.vicente98@outlook.com>
2021-12-28 17:05:37 -0300

    adicionando o TSringe para injeçao de dependencia

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+import { container } from "tsyringe";
 
 class CreateCategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
-
-    this.createCategoryUseCase.execute({ name, description });
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+    createCategoryUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,25 @@
 import { Router } from "express";
 import multer from "multer";
-import createCategoryController  from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategory";
 
 const categoriesRoutes = Router();
 
 const upload = multer({
-  dest:"./tmp"
+  dest: "./tmp",
 });
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoryController.handle(request, response);
 });
 
-categoriesRoutes.post("/import", upload.single("file"),(request, response) => {
-  return importCategoryController.handle(request,response)
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
