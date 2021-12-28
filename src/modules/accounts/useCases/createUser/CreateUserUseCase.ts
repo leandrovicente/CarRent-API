a2973428673b9c886950f6aeb0d089eb4ed6acd3
leandro <leandro.vicente98@outlook.com>
2021-12-28 18:59:50 -0300

    add hash password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import{ hash } from "bcrypt"
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -16,11 +17,15 @@ class CreateUserUseCase {
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
+
+    const passwordHash = await hash(password,8);
+
+
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password,
+      password:passwordHash,
     });
   }
 }
