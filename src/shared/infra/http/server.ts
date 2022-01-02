80fac38b38887a18c563a7a8b59ae4b1e4c676c2
leandro <leandro.vicente98@outlook.com>
2022-01-02 19:40:34 -0300

    refatorando

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,12 @@
 import express, { request, response } from "express";
 import swaggerUi from "swagger-ui-express";
-import "./database";
+import "@shared/infra/typeorm";
 import "@shared/container";
 import "express-async-errors";
-import { router } from "./routes";
-import swaggerFile from "./swagger.json";
+import { router } from "@shared/infra/http/routes";
+import swaggerFile from "../../../swagger.json";
 import { Request, Response, NextFunction } from "express";
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 
